b8cc7c4e772744911c95e4c6000b867d4cecad84
tiayo <656861622@qq.com>
2017-10-25 21:31:03 +0800

    修改

--- resources/views/home/index/index.blade.php
+++ resources/views/home/index/index.blade.php
@@ -2,45 +2,6 @@
 
 @section('title', '首页')
 
-@section('style')
-    <style type="text/css">
-        .banner {
-            width: 1000px;
-            margin: 50px auto;
-        }
-        .banner .banner-left {
-            float: left;
-            width: 150px;
-            box-shadow: 3px 3px #999,
-            -3px -3px #999;
-        }
-        .banner .banner-left li {
-            white-space:nowrap;
-            overflow:hidden;
-            text-overflow:ellipsis;
-            width: 70%;
-            height: 40px;
-            margin: 0 auto;
-            border-bottom: 1px dashed #000;
-            font-size: 16px;
-            text-align: center;
-            line-height: 40px;
-        }
-        .banner .banner-left li:nth-last-child(1) {
-            border-bottom: 0;
-        }
-        .banner .banner-right {
-            float: right;
-            width: 825px;
-        }
-        .banner .banner-right img {
-            float: left;
-            width: 100%;
-            height: 100%;
-        }
-    </style>
-@endsection
-
 @section('body')
     <div class="index">
         @include('home.layouts.header')
